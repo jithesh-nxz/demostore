8c5b7eb1262182805656edbd510a18f80077f6f1
Jithesh Rajan <jithesh.rajan@zenda.com>
2025-12-25 08:24:00 +0530

    change to npm start

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ npx medusa db:migrate
 echo "Seeding database..."
 npm run seed || echo "Seeding failed, continuing..."
 
-echo "Starting Medusa development server..."
-npm run dev
+echo "Starting Medusa production server..."
+npm run start
